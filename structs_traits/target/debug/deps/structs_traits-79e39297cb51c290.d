6d38a13fe6ada9ff17a76e99f4bdb3f304c2b3f0
obddd <obaidulhaq20@gmail.com>
2022-09-03 00:04:21 +0500

    created structs and trait

---
+++ structs_traits/target/debug/deps/structs_traits-79e39297cb51c290.d
@@ -0,0 +1,5 @@
+/home/obaid/ultimate_rust_crash_course/structs_traits/target/debug/deps/structs_traits-79e39297cb51c290: src/main.rs
+
+/home/obaid/ultimate_rust_crash_course/structs_traits/target/debug/deps/structs_traits-79e39297cb51c290.d: src/main.rs
+
+src/main.rs:
---
+++ structs_traits/target/debug/structs_traits.d
@@ -0,0 +1,1 @@
+/home/obaid/ultimate_rust_crash_course/structs_traits/target/debug/structs_traits: /home/obaid/ultimate_rust_crash_course/structs_traits/src/main.rs
